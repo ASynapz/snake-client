7048f42794f092831db80d59835336d78e4a64ba
Alan Wu <59207578+ASynapz@users.noreply.github.com>
2020-04-08 12:38:53 -0700

    Add files via upload

--- client.js
+++ client.js
@@ -1,31 +1,27 @@
 const net = require("net");
 const { IP, PORT } = require("./constants");
 
-const connect = function() {
+const connect = () => {
   const conn = net.createConnection({
-    host: IP
-    // host: "10.0.2.15",
-    port: PORT
+    host: '50.64.116.162',
+    port: 50541
+  });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to the game server");
+    conn.write("Name: FULLSEND");
+    // conn.write("Move: up");
+  });
+
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
   });
 
   conn.setEncoding("utf8");
 
-  conn.on("data", data => {
-    console.log("Server says: ", data);
-  });
-
-  conn.on("connect", () => {
-    console.log("Succefully connected to game server.");
-    // setInterval(() => {
-    //   conn.write("Move: up");
-    // }, 1000); // - move up one square (unless facing down)
-    conn.write("Name: FULLSEND");
-  });
   return conn;
 };
 
-module.exports = { connect };
-
-// "Move: up" - move up one square (unless facing down)
-// "Move: down" - move down one square (unless facing up)
-// "Move: left" - move left one square (unless facing right)
+module.exports = {
+  connect
+};
--- play.js
+++ play.js
@@ -1,18 +1,5 @@
-const net = require('net');
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({
-    host: '50.64.116.162' ,
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-
-  return conn;
-}
-
-console.log('Connecting ...');
-connect();
+console.log("Connecting...");
+setupInput(connect());
